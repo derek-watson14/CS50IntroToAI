be2d755df1ec01fdbe787f9588c199c7b28d8d38
Derek Watson <derek.watson92@gmail.com>
2021-09-04 15:06:20 -0700

    implemented update_q_value function

--- Week 4 - Learning/nim/nim.py
+++ Week 4 - Learning/nim/nim.py
@@ -122,7 +122,8 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        raise NotImplementedError
+        key = (tuple(state), action)
+        return old_q + self.alpha * ((reward + future_rewards) - self.q[key])
 
     def best_future_reward(self, state):
         """
